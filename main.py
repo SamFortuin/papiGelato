21759aa0d5b20cd644fe9da1df9f0542d503abb7
Sam Fortuin <99059050@mydavinci.nl>
2021-11-23 11:49:15 +0100

    finished papi

--- main.py
+++ main.py
@@ -1,4 +1,58 @@
-#import 
-def main():
-    pass
+#imported from other code but relative import be acting wack
+def intConvert(num):
+    numConvert = False
+    for i in range(10):
+        if str(i) in num:
+            numConvert = True
+    if numConvert == True:
+        return int(num)
+    else:
+        return num
+
+
+
+def main(abiMode=False):
+    bolAantal = intConvert(input('Hoeveel boljes wilt u?\n'))
+    #bolAantal = intConvert(bolAantal)
+    if str(type(bolAantal)) == "<class 'int'>":
+        if bolAantal <= 3 and bolAantal >= 1:
+            bakOfHoorn = input(f'Wilt u deze {bolAantal} bolletje(s) in \nA) een hoorntje of \nB) een bakje?\n').lower().replace("een","").replace("a","hoorntje",1).replace("b","bakje",1)
+            if bakOfHoorn == "hoorntje" or bakOfHoorn == "bakje":
+                print(f'Hier is uw {bakOfHoorn} met {bolAantal} bolletje(s)')
+            else:
+                if abiMode:
+                    if bakOfHoorn == "nee":
+                        print("Dan krijg je zo abi.")
+                    else:
+                        print('Daar kan ik niks mee abi, ik geef je gewoon zo mee.')
+                else:
+                    print('Dat is geen bakje of hoortje')
+        elif bolAantal >= 4 and bolAantal <= 8:
+            print(f'Dan krijgt u van mij een bakje met {bolAantal} bolletjes')
+        elif bolAantal >= 9:
+            print('Sorry, zulke grote bakken hebben we niet')
+        elif bolAantal == 0:
+            again = input('Wilt u geen ijsje?').lower().replace('a','').replace('ee','')
+            if again == 'n':
+                print('Okay, fijne dag verder.')
+                exit()
+            elif again == 'j':
+                main()
+            else:
+                print("invalid input")
+                main()
+        again = input('Wilt u nog meer bestellen? (J/N)').lower()
+        if again == 'n':
+            print('Okay, fijne dag verder.')
+            exit()
+        elif again == 'j':
+            main()
+        else:
+            print("invalid input")
+            main()
+    else:
+        print("Niet een getal, probeer het opnieuw.")
+        main()
+
 print('Welkom bij Papi Gelato je mag alle smaken kiezen zolang het maar vanille ijs is.')
+main(True)
