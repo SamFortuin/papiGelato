9b0ff1d30602ed145e5ad25e4ec58e5e9ca2e138
Sam Fortuin <99059050@mydavinci.nl>
2021-12-16 13:56:37 +0100

    added nakijkformulier and screencaps

--- test.py
+++
@@ -1,8 +0,0 @@
-# list2 = 'a boy came to giorgia'.split()
-# listString = ''
-# for i in range(len(list2)):
-#     list2[i] = list2[i].capitalize()
-#     listString += list2[i]+' '
-# print(listString) #prints 'A Boy Came To Giorgia'
-print(len(input("")))
-
